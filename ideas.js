0e6ba6c6d2be43e204720ff466f0ef6caf956d3a
Alex Sexton <AlexSexton@gmail.com>
2012-01-23 05:43:42 -0600

    Moving things around for docs location change.

--- ideas.js
+++
@@ -1,9 +0,0 @@
-var i18n = new Jed( locale_data );
-
-i18n.translate('I have a ticket.')
-      .onDomain('messages')
-      .withContext('male')
-      .ifPlural(5, 'I have %d$1 tickets.')
-      .fetch( sprintf_array );
-
-      // "I have 5 tickets."
